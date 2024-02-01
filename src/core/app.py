37fd0150c0d5192616db96ffaaa270b2cf9c5e30
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2024-02-01 12:00:43 -0600

    Added method to read config file to App class

--- src/core/app.py
+++ src/core/app.py
@@ -1,3 +1,4 @@
+import configparser
 import tkinter as tk
 
 
@@ -5,8 +6,30 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        # Load settings from config file
+        self.settings = self.read_config()
+
         self.title("iRacing Safety Car Generator")
         self.geometry("300x200")
 
         self.label = tk.Label(self, text="Test")
         self.label.pack(pady=50)
+
+    def read_config(self, file="settings.ini"):
+        # Initialize configparser
+        config = configparser.ConfigParser()
+        config.read(file)
+
+        # Create a dictionary to store the settings
+        settings = {
+            "min_safety_cars": config.getint("settings", "min_safety_cars"),
+            "max_safety_cars": config.getint("settings", "max_safety_cars"),
+            "start_minute": config.getfloat("settings", "start_minute"),
+            "end_minute": config.getfloat("settings", "end_minute"),
+            "min_time_between": config.getfloat("settings", "min_time_between"),
+            "laps_under_sc": config.getint("settings", "laps_under_sc")
+        }
+
+        # Return the settings
+        return settings
+
