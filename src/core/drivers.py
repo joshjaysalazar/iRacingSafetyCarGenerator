abcaac9ef0a02ca528ff48aa10d56118cb2a1edc
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2024-04-01 16:20:28 -0500

    Wrote the _update method for Drivers

--- src/core/drivers.py
+++ src/core/drivers.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import time
 
 
 class Drivers:
@@ -6,8 +7,11 @@ class Drivers:
         self.master = master
 
         # Dictionaries to track the state of the drivers
-        self.current_drivers = {}
-        self.previous_drivers = {}
+        self.current_drivers = []
+        self.previous_drivers = []
+
+        # Do the initial update
+        self._update()
 
     def _loop(self):
         pass
@@ -16,8 +20,23 @@ class Drivers:
         # Copy the current drivers to the previous drivers
         self.previous_drivers = deepcopy(self.current_drivers)
 
-        # Update the current drivers
-        
+        # Clear the current drivers
+        self.current_drivers = []
+
+        # Gather the updated driver data
+        in_pits = self.master.ir["CarIdxOnPitRoad"]
+        track_surface = self.master.ir["CarIdxTrackSurfaceMaterial"]
+        lap_distance = self.master.ir["CarIdxLapDistPct"]
+
+        # Organize the updated driver data and update the current drivers
+        for i in range(len(in_pits)):
+            self.current_drivers.append(
+                {
+                "in_pits": in_pits[i],
+                "track_surface": track_surface[i],
+                "lap_distance": lap_distance[i]
+                }
+            )
 
     def run(self):
         pass
