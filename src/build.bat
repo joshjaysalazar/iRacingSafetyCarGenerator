fd736720615f1aa4c78037481b534b5b2d935896
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2024-10-16 15:47:20 -0400

    Fix executable name on build

--- src/build.bat
+++ src/build.bat
@@ -4,7 +4,7 @@ ECHO Building binary. Please wait...
 :: Import pywinauto before building to avoid missing library error
 python -c "import pywinauto"
 :: Build new binary
-pyinstaller --noconfirm --log-level=FATAL --noconsole --onefile --hidden-import=comtypes.gen.UIAutomationClient main.py
+pyinstaller --noconfirm --log-level=FATAL --noconsole --onefile --hidden-import=comtypes.gen.UIAutomationClient --name=iRSCG main.py
 
 ECHO Copying files to dist...
 :: Copy settings.ini to dist
