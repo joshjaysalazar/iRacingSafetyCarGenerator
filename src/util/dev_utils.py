520faf123e2a0ecd07cd1b461b935d0871922ea5
Lars Tijhuis <ljtijhuis@gmail.com>
2025-03-08 00:18:40 -0500

    Add dev button to copy a snapshot of the SDK data to clipboard

---
+++ src/util/dev_utils.py
@@ -0,0 +1,30 @@
+import irsdk
+import json
+import pyperclip
+
+def copy_sdk_data_to_clipboard():
+    ir = irsdk.IRSDK()
+    connected = False
+    try:
+        if not ir.startup():
+            pyperclip.copy("Could not connect through the SDK")
+            return
+        
+        connected = True
+        
+        data = {
+            "SessionNum": ir["SessionNum"],
+            "SessionInfo_Sessions": ir["SessionInfo"]["Sessions"],
+            "SessionFlags": ir["SessionFlags"],
+            "DriverInfo_Drivers": ir["DriverInfo"]["Drivers"],
+            "CarIdxLap": ir["CarIdxLap"],
+            "CarIdxLapDistPct": ir["CarIdxLapDistPct"],
+            "CarIdxClass": ir["CarIdxClass"],
+            "CarIdxOnPitRoad": ir["CarIdxOnPitRoad"],
+        }
+        
+        pyperclip.copy(json.dumps(data))
+
+    finally:
+        if connected:
+            ir.shutdown()
