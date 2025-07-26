6d94bf01d126ae8facb0aedf17d445d69f62e06b
Lars Tijhuis <ljtijhuis@gmail.com>
2025-07-26 19:40:52 -0400

    Add off track detector class

---
+++ src/core/detection/off_track_detector.py
@@ -0,0 +1,23 @@
+from irsdk import TrkLoc
+
+from core.drivers import Drivers
+
+class OffTrackDetector:
+    def detect(self, drivers: Drivers):
+        """Detect if any driver is off track.
+
+        Args:
+            drivers (Drivers): The Drivers object containing the current state of drivers.
+
+        Returns:
+            list: A list of driver indices that are off track.
+        """
+        off_track_drivers = []
+        for driver in drivers.current_drivers:
+            if driver["track_loc"] == TrkLoc.off_track and \
+               driver["laps_completed"] >= 0:
+               
+               # driver is off track and is active in the session
+               off_track_drivers.append(driver)
+        
+        return off_track_drivers
